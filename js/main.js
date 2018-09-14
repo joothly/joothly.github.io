f8bff3ea9cd2ba6f9c017a56537704f19e77eb49
joothly <joothly@gmail.com>
2018-09-14 16:09:14 +0200

    add new font + streamline ui

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-// hand
-
-const _hand = document.querySelector('.hand-button')
-const activeClass = 'page--active'
-
-_hand.addEventListener('click', () => {
-  const _pageIntro = document.querySelector('.page--intro')
-  const _pageHome = document.querySelector('.page--home')
-
-  _pageIntro.classList.remove(activeClass)
-  _pageHome.classList.add(activeClass)
-})
-
 // easter egg
 
 const _y = document.querySelector('.title__letter--y')
